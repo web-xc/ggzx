92e1da8332cff3df49a6ac6d8bc0df6f75fd839b
web-xc <2848141597@qq.com>
2023-07-26 03:47:55 +0800

    测试commitlint配置

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 const fn = () => {
-  console.log('测试没有格式化的代码')
+  console.log("测试没有格式化的代码")
 }
 fn()
 
 const fnn = () => {
-  console.log('1111')
+  console.log("1111")
 }
