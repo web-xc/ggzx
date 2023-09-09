e64964f0d044ca1062241f9c900831ffa0c9821d
web-xc <2848141597@qq.com>
2023-09-09 18:11:39 +0800

    整理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,7 +5,7 @@
             <el-col :span="12" :xs="24">
                 <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
                     <h1>Hello</h1>
-                    <h2>欢迎来到硅谷甄选</h2>
+                    <h2>欢迎来到汇客运营平台</h2>
                         <el-form-item prop="username">
                             <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
                         </el-form-item>
